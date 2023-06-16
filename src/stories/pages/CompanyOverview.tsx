6b97dd06e8ff11601023958da164ceb47c4e8fa5
Henrique Hallmann <henriquev@atlashxm.com>
2023-06-16 15:25:12 -0300

    make company overview page work

--- src/stories/pages/CompanyOverview.tsx
+++ src/stories/pages/CompanyOverview.tsx
@@ -3,9 +3,22 @@ import { SubHeader } from "../../layout/SubHeader/SubHeader"
 import { Sidebar } from "../../layout/Sidebar/Sidebar"
 
 export const CompanyOverview = () => (
-    <article>
-        <Header />
-        <SubHeader />
-        <Sidebar />
-    </article>
+  <section>
+    <Header />
+    <div className="flex pt-1 overflow-hidden bg-gray-50 dark:bg-gray-900">
+      <Sidebar />
+      <div id="main-content" className="relative w-full h-full overflow-y-auto bg-gray-50 lg:ml-1 dark:bg-gray-900">
+        <main>
+          <SubHeader />
+        </main>
+        <span>hello footer</span>
+      </div>
+    </div>
+  </section>
+    // <section>
+    //   <Header />
+    //   
+      
+    //   <SubHeader />
+    // </section>
 )
